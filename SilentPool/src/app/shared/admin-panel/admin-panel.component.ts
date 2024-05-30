2cb84ea1960827bbdb97b08aa2aff759bed3d37a
Luiz Fernando Meneses Pereira <luizfernandomdp14@outlook.com>
2024-05-30 16:21:49 -0300

    ajustes

--- SilentPool/src/app/shared/admin-panel/admin-panel.component.ts
+++ SilentPool/src/app/shared/admin-panel/admin-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-panel',
@@ -13,4 +14,20 @@ export class AdminPanelComponent {
   onBaixarPerguntas() {
     this.baixarPerguntas.emit();
   }
+
+  BaixarButton: boolean = false;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.route.url.subscribe(url => {
+      const currentUrl = this.router.url;
+      this.BaixarButton = this.shouldButtonBeHiddenBasedOnUrl(currentUrl);
+    });
+  }
+
+  shouldButtonBeHiddenBasedOnUrl(url: string): boolean {
+    return url.includes('/perguntas');
+  }
+  
 }
